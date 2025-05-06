37db66bf2136522d6a03cfe7fa6ec4249c8da0f2
Yudhriz <riskyyudha678@gmail.com>
2025-05-06 15:53:44 +0700

    feat(register): add register page and routes

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Login from "../pages/login/Login";
+import Register from "../pages/register/Register";
 
 export default function AppRoutes() {
   return (
     <Routes>
       <Route path='/' element={ <Home /> } />
       <Route path='/login' element={ <Login /> } />
+      <Route path='/register' element={ <Register /> } />
     </Routes>
   );
 }
