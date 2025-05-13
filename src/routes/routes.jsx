e93edc04c696c70f985a8003c7187c2a4da7bfff
Yudhriz <riskyyudha678@gmail.com>
2025-05-13 14:34:31 +0700

    fear(🚫404): add not found 404 page

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,15 +4,17 @@ import Login from "../pages/login/Login";
 import Register from "../pages/register/Register";
 import About from "../pages/about/About";
 import Docs from "../pages/docs/Docs";
+import NotFound from "../pages/404/NotFound";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path='/' element={ <Home /> } />
-      <Route path='/login' element={ <Login /> } />
-      <Route path='/register' element={ <Register /> } />
-      <Route path='/about' element={ <About /> } />
-      <Route path='/docs' element={ <Docs /> } />
+      <Route path='/' element={<Home />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='/about' element={<About />} />
+      <Route path='/docs' element={<Docs />} />
+      <Route path='*' element={<NotFound />} />
     </Routes>
   );
 }
